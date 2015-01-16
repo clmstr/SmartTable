d2f680a62c2f895a2ef5a2d388bc5c9f693e341e
coolmaister <coolmaister@gmail.com>
2015-01-16 01:29:09 +0100

    bf

--- WebContent/js/controllers/ElementChooserCtrl.js
+++ WebContent/js/controllers/ElementChooserCtrl.js
@@ -2,31 +2,21 @@ smarttableApp.controller('ElementChooserCtrl',['$scope',
                                              '$location',
      function ($scope, 
     		 $location){
-		 $scope.pic1 = {src: 'images/3_1/deszertkes.png',
-			 			text:'DESZERTKÉS'};
-		 $scope.pic1Cont = {};
-		 $scope.pic2 = {src: 'images/3_1/evokes.png',
-			 				text:'EVŐKÉS'};
-		 $scope.pic2Cont = {};
-		 $scope.pic3 = {src: 'images/3_1/halkes.png',
-	 				text:'HALKÉS'};
-		 $scope.pic3Cont = {};
-		 
-		 
-		 //cutterlyquiz_2
-		 $scope.pic4 = {src: 'images/3_2/deszertvilla.png',
-			 			text:'DESZERTVILLA'};
-		 $scope.pic4Cont = {};
-		 $scope.pic5 = {src: 'images/3_2/evovilla.png',
-			 				text:'EVŐVILLA'};
-		 $scope.pic5Cont = {};
-		 $scope.pic6 = {src: 'images/3_2/halvilla.png',
-					text:'HALVILLA'};
-		 $scope.pic6Cont = {};
+		
+	
+	
+		$scope.formats = [100,200,30,400];
 		 
 		 
 		 $scope.nextPage = function(view){
-			 $location.path(view);
+			 
+			 if (($scope.format1 == 100)&&
+					 ($scope.format2 == 200)&&
+					 ($scope.format3 == 30)&&
+					 ($scope.format4 == 400)
+			 
+			 )
+				 $location.path(view);
 		 }
 		 
 	
